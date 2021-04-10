162406efe1b84e0ec078fc25e946a894d07dc9ee
Дмитрий Анфимов <mrrobot@MacBook-Pro-Dmitrij.local>
2021-04-10 06:37:58 +0300

    HTML + CSS

--- 3057401/djangoProject/settings.py
+++ 3057401/djangoProject/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
